f80a9ed46109a237d75b441d3eded9d0a871b036
MarieAnneClay <marie-anne.clay@epfedu.fr>
2017-10-10 16:26:36 +0200

    Delete 2-PRIVILEGES.sql

--- javaTraining/docker/mysqlProd/2-PRIVILEGES.sql
+++
@@ -1,8 +0,0 @@
-  #-----------------------------------
-  #USER RIGHTS MANAGEMENT
-  #-----------------------------------
-  CREATE USER 'admincdb'@'%' IDENTIFIED BY 'qwerty1234';
-
-  GRANT ALL ON `computer-database-db`.* TO 'admincdb'@'%' WITH GRANT OPTION;
-
-FLUSH PRIVILEGES;
