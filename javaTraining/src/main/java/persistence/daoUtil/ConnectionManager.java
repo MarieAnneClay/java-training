a28195a2b8866c98e0731d6f9141833aedb472c0
MarieAnneClay <marie-anne.clay@epfedu.fr>
2017-10-11 11:28:06 +0200

    [FEAT] Spring with HickariCP

--- javaTraining/src/main/java/persistence/daoUtil/ConnectionManager.java
+++ javaTraining/src/main/java/persistence/daoUtil/ConnectionManager.java
@@ -3,13 +3,14 @@ package persistence.daoUtil;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectionManager {
 
     private static HikariDataSource ds;
-    private static final String FILE_CONFIG = "/db.properties";
     private static final ConnectionManager INSTANCE = new ConnectionManager();
 
     /** Point d'accès pour l'instance unique du singleton.
@@ -21,8 +22,9 @@ public class ConnectionManager {
     /** CONSTRUCTOR.
      * @throws DAOConfigurationException a DAO exception (src/main/daoUtil) */
     private ConnectionManager() throws DAOConfigurationException {
-        HikariConfig cfg = new HikariConfig(FILE_CONFIG);
-        ds = new HikariDataSource(cfg);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        ds = new HikariDataSource((HikariConfig) context.getBean("dataSource"));
+        context.close();
     }
 
     /** function which connect to the database.
